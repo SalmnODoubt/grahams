cde791c074cbfdd29a6b7d6595ae744d66bd7022
Jonny Biederstädt <johannes.biederstaedt@tamedia.ch>
2016-01-12 10:26:34 +0100

    Initial Commit

---
+++ GrahamsNumber/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GrahamsNumber
+//
+//  Created by Jonny Biederstädt on 12/01/16.
+//  Copyright © 2016 Jonny Biederstädt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
